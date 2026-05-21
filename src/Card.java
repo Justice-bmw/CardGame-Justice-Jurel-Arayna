9cd2f475d995fef1b0f25d5a5e45a6d88babcfba
HelloBumski <jurelc@live.wsd1.org>
2026-05-21 11:49:50 -0500

    changed comments

---
+++ src/Card.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Base class for all cards in the game.
+ */
+public class Card {
+    public void play(Player currentPlayer, ArrayList<Player> allPlayers, ArrayList<Card> deck) {};
+    
+}
